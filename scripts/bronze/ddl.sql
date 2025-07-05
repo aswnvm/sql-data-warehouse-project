94f6475c162161b0551ccefade5b2b809017d02c
aswn <aswnvm@gmail.com>
2025-07-05 11:52:29 +0530

    added data integration diagram

--- scripts/bronze/ddl.sql
+++ scripts/bronze/ddl.sql
@@ -34,11 +34,11 @@ CREATE TABLE bronze.crm_prd_info(
 );
 GO
 
-IF OBJECT_ID('bronze.sales_details', 'U') IS NOT NULL
-	DROP TABLE bronze.sales_details;
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
 GO
 
-CREATE TABLE bronze.sales_details(
+CREATE TABLE bronze.crm_sales_details(
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
